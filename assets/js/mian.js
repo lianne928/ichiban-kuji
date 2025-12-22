4e008d7518b35a34c50ff649b206754139f7d5a6
lianne928 <lianne.14928@gmail.com>
2025-12-22 09:11:22 +0800

    style: refine main links and borders

--- assets/js/mian.js
+++ assets/js/mian.js
@@ -4,28 +4,28 @@ let carouselImgData = [
     img: "../assets/image/eventPuppet.jpg",
     tittle: "PUPPET SUNSUN 帕比順順",
     desc: "如果你正在找一個「陪你生活」的角色，那首選就是PUPPETSUNSUN!",
-    href: "https://yokoku.pass-the-baton.com/",
+    href: "./event_detail_sunsun.html",
   }),
   // 七龍珠
   (eventBanpresto = {
     img: "../assets/image/event_BANPRESTO.jpg",
     tittle: "七龍珠 THE CHRONICLE OFGOKU』童年悟空、大猿悟空",
     desc: "明年五月開抽！",
-    href: "https://yokoku.pass-the-baton.com/",
+    href: "./event_detail_sunsun.html",
   }),
   // MOS
   (eventMos = {
     img: "../assets/image/eventMos.jpg",
     tittle: "《摩斯漢堡》第二彈開抽！",
     desc: "Last 賞 10 抽倒數中！摩斯漢堡造型的可愛時鐘，還能播放「摩斯店內音樂」",
-    href: "https://yokoku.pass-the-baton.com/",
+    href: "./event_detail_sunsun.html",
   }),
   // Princess Kaguya
   (eventPrincessKaguya = {
     img: "../assets/image/eventPrincessKaguya.jpg",
     tittle: "超時空輝耀姬！（超かぐや姫！/ Cosmic Princess Kaguya!）",
     desc: "將於2026年1月22日於 Netflix 全球獨家播出。",
-    href: "https://yokoku.pass-the-baton.com/",
+    href: "./event_detail_sunsun.html",
   }),
 ];
 
